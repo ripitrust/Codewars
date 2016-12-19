18ed59a1cf8b84ea55e8fc943280c393fc187d8e
yangfan <yangfan@garena.com>
2016-12-19 17:17:57 +0800

    remove space text_justify

--- Python/text_justify.py
+++ Python/text_justify.py
@@ -1,41 +1,6 @@
-
-def justify(text, width):
-    words = text.split()
-    maxWidth = width
-    res, cur, num_of_letters = [], [], 0
-    for w in words:
-        if num_of_letters + len(w) + len(cur) > maxWidth:
-            for i in range(maxWidth - num_of_letters):
-                cur[i % (len(cur) - 1 or 1)] += ' '
-            res.append(''.join(cur))
-            cur, num_of_letters = [], 0
-        cur += [w]
-        num_of_letters += len(w)
-
-        result = res + [' '.join(cur).ljust(maxWidth)]
-
-    result[len(result) - 1] = ' '.join(result[len(result) - 1].split())
-    new_result = map(lambda s: s.rstrip(), result)
-    return '\n'.join(new_result)
-
-
-def justify_best_practice(text, width):
-    length = text.rfind(' ', 0, width+1)
-    if length == -1 or len(text) <= width: return text
-    line = text[:length]
-    spaces = line.count(' ')
-    if spaces != 0:
-        expand = (width - length) / spaces + 1
-        extra = (width - length) % spaces
-        line = line.replace(' ', ' '*expand)
-        line = line.replace(' '*expand, ' '*(expand+1), extra)
-    return line + '\n' + justify(text[length+1:], width)
-
-
 INF = float('inf')
 
-
-def justify2(text, width):
+def justify(text, width):
 
     """
     the c array in ths function is the smallest sum cost of justification,
@@ -50,7 +15,6 @@ def justify2(text, width):
     or from start to end
 
     in the case of cubed cost function, only from end to start works
-
 
     """
 
@@ -67,7 +31,7 @@ def justify2(text, width):
 
             if badness <= c[i] and badness != INF:
                 c[i] = badness
-            # if badness < c[j]: #update parent pointer
+            # if badness < c[j]: update parent pointer
                 p[i] = j
 
     return c, p
@@ -81,7 +45,7 @@ def cost(words, width, i, j):
       it means that the space for each line must be as even as possible,  since one space for each line costs much less
       than three space for one line and zero for two other lines:
 
-            1 ** 3 + 1 ** 3 + 1** 3 = 3  <<  3 ** 3 + 0 + ) = 27
+            (1 ** 3 + 1 ** 3 + 1 ** 3) = 3  <<  (3 ** 3 + 0 + 0) = 27
 
     So if the cost function is cubed, the algorithm will try to spread the word as even as possible, but when not cubed,
 
@@ -96,7 +60,6 @@ def cost(words, width, i, j):
     if length + num_space > width:
         return INF
     return (width - length - num_space) ** 3
-
 
 
 def print_wrap(p, text, width):
@@ -148,6 +111,38 @@ def print_wrap_end(p, text, width):
     return result[::-1]
 
 
+def justify_ref(text, width):
+    words = text.split()
+    maxWidth = width
+    res, cur, num_of_letters = [], [], 0
+    for w in words:
+        if num_of_letters + len(w) + len(cur) > maxWidth:
+            for i in range(maxWidth - num_of_letters):
+                cur[i % (len(cur) - 1 or 1)] += ' '
+            res.append(''.join(cur))
+            cur, num_of_letters = [], 0
+        cur += [w]
+        num_of_letters += len(w)
+
+        result = res + [' '.join(cur).ljust(maxWidth)]
+
+    result[len(result) - 1] = ' '.join(result[len(result) - 1].split())
+    new_result = map(lambda s: s.rstrip(), result)
+    return '\n'.join(new_result)
+
+
+def justify_best_practice(text, width):
+    length = text.rfind(' ', 0, width+1)
+    if length == -1 or len(text) <= width: return text
+    line = text[:length]
+    spaces = line.count(' ')
+    if spaces != 0:
+        expand = (width - length) / spaces + 1
+        extra = (width - length) % spaces
+        line = line.replace(' ', ' '*expand)
+        line = line.replace(' '*expand, ' '*(expand+1), extra)
+    return line + '\n' + justify(text[length+1:], width)
+
 if __name__ == '__main__':
 
     # text = 'a b c d e'.split()
@@ -159,16 +154,16 @@ if __name__ == '__main__':
     # text = 'what must be shall be.'.split()
 
     # text = 'hello world come to the dark side of the force'.split()
-    width = 20
-    c, p = justify2(text, width)
-    print c
-    print p
-
-    print "Ref".center(width, "-")
-    print justify_best_practice(' '.join(text), width)
-    print "=".center(width, "-")
-
+    width = 25
+    c, p = justify(text, width)
     result = print_wrap_end(p, text, width)
+    # print c
+    # print p
+
+    # print "Ref".center(width, "-")
+    # print justify_best_practice(' '.join(text), width)
+    # print "=".center(width, "-")
+
 
     # result[len(result) - 1] = (' '.join(result[len(result) - 1].split())).ljust(width)  #only works for leetcode
 
